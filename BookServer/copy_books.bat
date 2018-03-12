6883f73a67fd575586f844d0ccec2645cd1b8afe
Yamamoto Naoya <yamamoto@kwiksher.com>
2018-03-12 12:38:11 +0900

    Copy bat for bookserver

--- BookServer/copy_books.bat
+++ BookServer/copy_books.bat
@@ -1,30 +1,27 @@
 set fol=.\bookshelf
 cd %HOMEPATH%\Documents\Kwik\BookServer
-call :MyCopyBgImg A_Direng_v1 book03
-call :MyCopyBook A_Direng_v1 book03xh
+call :MyCopy A_Direng_v1 book03 xh true
 exit /b
 
-:MyCopyBook
-echo MyCopyBook %1
+:MyCopy
+echo MyCopy %1
 copy  copyright.txt ..\%1\build4\assets\copyright.txt
 copy ..\%1\build4\model.json  ..\%1\build4\assets\model.json
 copy ..\%1\*.mp3 ..\%1\build4\assets\audios\
 copy ..\%1\*.txt ..\%1\build4\assets\audios\
 cd ..\%1\build4
-move assets %2
+move assets %2%3
 rm ..\assets.zip
-"C:\Program Files (x86)\Corona Labs\Corona SDK\7za.exe" a ..\assets.zip %2
-move %2 assets
+"C:\Program Files (x86)\Corona Labs\Corona SDK\7za.exe" a ..\assets.zip %2%3
+move %2%3 assets
 cd %~dp0
-mkdir  %fol%\%2
-copy ..\%1\assets.zip %fol%\%2\assets.zip
-copy ..\%1\build4\assets\images\p1\bg.png  %fol%\%2\bg.png
-exit /b
-
-:MyCopyBgImg
-echo MyCopyBgImg %1
-cd %~dp0
-mkdir  %fol%\%2
-copy ..\%1\build4\assets\images\p1\bg.png  %fol%\%2\bg.png
+mkdir  %fol%\%2%3
+copy ..\%1\assets.zip %fol%\%2%3\assets.zip
+copy ..\%1\build4\assets\images\p1\bg.png  %fol%\%2%3\bg.png
+echo %4
+if "%4"=="true" (
+  mkdir  %fol%\%2
+  copy ..\%1\build4\assets\images\p1\bg.png  %fol%\%2\bg.png
+)
 
 exit /b
